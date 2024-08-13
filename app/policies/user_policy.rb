7750b0944873749506032018fa930f918b5adb68
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-08-13 22:05:01 +0900

    feat: user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -10,23 +10,23 @@ class UserPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+  end
 
-    def edit?
-      user_is_owner?
-    end
+  def edit?
+    user_is_owner?
+  end
 
-    def update?
-      user_is_owner?
-    end
+  def update?
+    user_is_owner?
+  end
 
-    def destroy?
-      user_is_owner?
-    end
+  def destroy?
+    user_is_owner?
+  end
 
-    private
+  private
 
-    def user_is_owner?
-      user == record
-    end
+  def user_is_owner?
+    user == record
   end
 end
