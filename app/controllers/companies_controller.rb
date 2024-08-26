81423e21d1f5677da01a42ca7c4481e89183e321
narumi katayama <narumikataiama@icloud.com>
2024-08-26 12:42:38 +0900

    heroku settings

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -78,6 +78,8 @@ class CompaniesController < ApplicationController
       :company_name, :company_register_number, :company_address, :company_city,
       :company_state, :company_zip_code, :company_country, :company_phone_number,
       :company_website, :company_description, :company_category,
+      :company_logo,
+      :company_cover,
       :company_contact_name, :company_contact_email, company_social_media: [:facebook, :twitter, :linkedin, :instagram, :youtube, :tiktok],
     )
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(
+      :avatar,
       :email,
       :password,
       :password_confirmation,
