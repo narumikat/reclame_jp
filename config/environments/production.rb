a521bf439b0067f8591963219cc41fc4f7e74e3d
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-09-12 13:48:40 +0900

    feat: email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,19 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              'mail.privateemail.com',
+    port:                 587,
+    domain:               'www.reclamejp.com',
+    user_name:            ENV["USER_EMAIL"],
+    password:             ENV["USER_EMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none'
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
