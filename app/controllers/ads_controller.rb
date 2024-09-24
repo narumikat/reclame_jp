3d5342b349caedd92313e293545efc321d6e03ce
narumi katayama <narumikataiama@icloud.com>
2024-09-24 22:25:10 +0900

    ads

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -24,6 +24,21 @@ class AdsController < ApplicationController
     @ad = Ad.find(params[:id])
   end
 
+  def edit
+    @ad = Ad.find(params[:id])
+  end
+
+  def update
+    authorize @ad
+    if @ad.update(ad_params)
+      redirect_to ads_path, notice: 'Anúncio atualizado com sucesso.'
+    else
+      flash[:alert] = 'Falha ao atualizar o anúncio: ' + @ad.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+    
+
   private
 
   def ad_params
